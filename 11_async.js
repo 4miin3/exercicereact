a58d7c3b007263ea11a45c55c7f54fc9bb243b76
Amine Bou <xenium@MacBook-Air-de-Amine.local>
2024-05-13 16:47:10 +0200

    exercice 11 12 test pas sur

--- 11_async.js
+++ 11_async.js
@@ -1,52 +1,25 @@
-//const {sleep} = require("../exercices/10_promise");
+const { sleep } = require(".../EXERCICES_START/10_promise.js");
+const axios = require("axios");
 
-/**
- * Créez une fonction synchrone qui attend 2 seconde puis execute le callback passé en paramètre
- * vous pouvez utiliser la fonction sleep créé précedement: const {sleep} = require("../exercices/10_promise");
- * 
- * Vous devez utiliser .then
- * 
- * contrainte:
- *    - votre fonction doit être synchrone et ne pas retourner de Promise
- *    - ne pas utiliser async await
- * 
- */
 const usingThen = (cb) => {
-}
+  sleep().then(() => {
+    cb();
+  });
+};
 
-/**
- * Créez une fonction asynchrone qui attend 2 seconde puis execute le callback passé en paramètre
- * vous pouvez utiliser la fonction sleep créé précedement: const {sleep} = require("../exercices/10_promise");
- * 
- * Vous devez utiliser await
- * 
- * contrainte:
- *   - votre fonction doit être asynchrone et retourner une Promise
- *   - ne pas utiliser .then
- */
-
-const usingAwait = (cb) => {
-
-}
-
-/**
- * Créez une fonction asynchrone qui effectue un appel api vers l'url passé en paramètre
- * retourne le résultat de la requête (body)
- * 
- * Vous pouvez utiliser axios, mais n'oubliez pas d'installer le package et de l'importer
- * npm install axios
- * 
- * votre réponse doit être un objet json
- * 
- * url de test: https://jsonplaceholder.typicode.com/todos/1
- */
-
-//décommentez la ligne suivante une fois le package installé
-//const axios = require("axios");
+const usingAwait = async (cb) => {
+  await sleep();
+  cb();
+};
 
 const apiResponse = async (url) => {
+  try {
+    const response = await axios.get(url);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw error;
+  }
+};
 
-}
-
-
-module.exports = {usingThen, usingAwait, apiResponse};
+module.exports = { usingThen, usingAwait, apiResponse };
--- 12_parallel.js
+++ 12_parallel.js
@@ -1,22 +1,22 @@
-/**
- * Utilisez la fonction Promise.all pour exécuter 3 appels API en parallèle
- * 
- * urls:
- * - https://jsonplaceholder.typicode.com/todos/1
- * - https://jsonplaceholder.typicode.com/todos/2
- * - https://jsonplaceholder.typicode.com/todos/3
- * 
- * Retournez un tableau contenant les 3 résultats
- * 
- * Vous pouvez utiliser votre fonction apiResponse créé précedement: const {apiResponse} = require("./11_async");
- * 
- * documentation: https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Global_Objects/Promise/all
-*/
+const { apiResponse } = require("./11_async");
 
-//const {apiResponse} = require("./11_async");
+const parallel = async () => {
+  const urls = [
+    "https://jsonplaceholder.typicode.com/todos/1",
+    "https://jsonplaceholder.typicode.com/todos/2",
+    "https://jsonplaceholder.typicode.com/todos/3"
+  ];
 
-const parallel = () => {
+  try {
+    // Map over each URL and call apiResponse asynchronously
+    const promises = urls.map(url => apiResponse(url));
 
-}
+    // Wait for all promises to resolve
+    return Promise.all(promises);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw error;
+  }
+};
 
-module.exports = {parallel};
+module.exports = { parallel };
